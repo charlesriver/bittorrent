04bc0a87aa37fa8eb0ef34ad743ab1d93ef59330
Kevin Lee <kevinlee01@college.harvard.edu>
2017-09-20 22:58:46 -0400

    premerge

--- klcztyrant.py
+++ klcztyrant.py
@@ -12,64 +12,11 @@ import logging
 from messages import Upload, Request
 from util import even_split
 from peer import Peer
+from klczstd import KlczStd
 
-class KlczTyrant(Peer):
-    def post_init(self):
-        print "post_init(): %s here!" % self.id
-        self.dummy_state = dict()
-        self.dummy_state["cake"] = "lie"
-    
-    def requests(self, peers, history):
-        """
-        peers: available info about the peers (who has what pieces)
-        history: what's happened so far as far as this peer can see
+class KlczTyrant(KlczStd):
 
-        returns: a list of Request() objects
-
-        This will be called after update_pieces() with the most recent state.
-        """
-        needed = lambda i: self.pieces[i] < self.conf.blocks_per_piece
-        needed_pieces = filter(needed, range(len(self.pieces)))
-        np_set = set(needed_pieces)  # sets support fast intersection ops.
-
-
-        logging.debug("%s here: still need pieces %s" % (
-            self.id, needed_pieces))
-
-        logging.debug("%s still here. Here are some peers:" % self.id)
-        for p in peers:
-            logging.debug("id: %s, available pieces: %s" % (p.id, p.available_pieces))
-
-        logging.debug("And look, I have my entire history available too:")
-        logging.debug("look at the AgentHistory class in history.py for details")
-        logging.debug(str(history))
-
-        requests = []   # We'll put all the things we want here
-        # Symmetry breaking is good...
-        random.shuffle(needed_pieces)
-        
-        # Sort peers by id.  This is probably not a useful sort, but other 
-        # sorts might be useful
-        peers.sort(key=lambda p: p.id)
-        # request all available pieces from all peers!
-        # (up to self.max_requests from each)
-        for peer in peers:
-            av_set = set(peer.available_pieces)
-            isect = av_set.intersection(np_set)
-            n = min(self.max_requests, len(isect))
-            # More symmetry breaking -- ask for random pieces.
-            # This would be the place to try fancier piece-requesting strategies
-            # to avoid getting the same thing from multiple peers at a time.
-            for piece_id in random.sample(isect, n):
-                # aha! The peer has this piece! Request it.
-                # which part of the piece do we need next?
-                # (must get the next-needed blocks in order)
-                start_block = self.pieces[piece_id]
-                r = Request(self.id, peer.id, piece_id, start_block)
-                requests.append(r)
-
-        return requests
-
+    # CHANGE THIS FOR BITTYRANT
     def uploads(self, requests, peers, history):
         """
         requests -- a list of the requests for this peer for this round
